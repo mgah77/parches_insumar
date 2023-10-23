c506d5f426062f30e2801fe3e8454583d57bdeee
mgah77 <mauricio.gah@gmail.com>
2023-10-23 16:11:50 -0300

    123

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,9 +1,7 @@
-from odoo import models, fields, _
-from odoo.exceptions import ValidationError
-
+from odoo import models, fields
 
 class SaleOrderCompany(models.Model):
     _inherit = 'sale.order'  # Reemplaza 'your.model' por el nombre del modelo que deseas modificar
 
     # Modifica el campo partner_id para agregar un filtro adicional
-    partner_id = fields.Many2one(domain="[('type', '!=', 'private'), ('company_id', 'in', (False, company_id)), ('is_company', '=', True)]"
+    partner_id = fields.Many2one(domain="[('type', '!=', 'private'), ('company_id', 'in', (False, company_id)), ('is_company', '=', True)]")
