5c308eb1a6381b7fe6e5ab9ae4b9b7035f4f8aeb
mgah77 <mauricio.gah@gmail.com>
2023-10-25 19:43:49 -0300

    Update stock_pico.py

--- models/stock_pico.py
+++ models/stock_pico.py
@@ -8,7 +8,7 @@ class stock_picking_kanban(models.Model):
     code = fields.Selection([('incoming', 'Receipt'), ('outgoing', 'Delivery'), ('internal', 'Internal Transfer')], 'Type of Operation', required=True)
     count_picking_draft = fields.Integer(default=1)
     count_picking_ready = fields.Integer(default=2)
-    count_picking_waiting = fields.Integer('Current User', default=lambda self: self.env.uid, (domain="[('active', '=', True)])
+    count_picking_waiting = fields.Integer('Current User', default=lambda self: self.env.uid, (domain="[('active', '=', True)]))
     count_picking_late = fields.Integer(default=4)
     count_picking_backorders = fields.Integer(default=5)
     color = fields.Integer(default=1)
