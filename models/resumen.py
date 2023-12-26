d5f86d0a19a63b82c8a523de1258f7dbaaacd68f
mgah77 <mauricio.gah@gmail.com>
2023-12-26 20:01:40 -0300

    Update resumen.py

--- models/resumen.py
+++ models/resumen.py
@@ -5,13 +5,12 @@ class Resumen(models.Model):
     _name = 'taller.resumen'
     _description = 'Resumen'
 
-    recibidas = fields.Integer('Recibidas')
+    recibidas = fields.Integer('Recibidas', readonly=True)
     por_pagar = fields.Integer('Por Pagar')
 
     @api.model
     def _compute_recibidas(self):
-        temp = ''     
-        for record in self:
-            temp = self.env['account.move'].search([('move_type','=','in_invoice'),('payment_state','=','not_paid')])           
-            record.recibidas = len(temp)
+        temp = self.env['account.move'].search_count([('move_type','=','in_invoice'),('payment_state','=','not_paid')])      
+        for record in self:                    
+            record.recibidas = temp
         return    
