5594d981e47879c965a677431f835f8b14e094d7
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-07-15 18:40:05 -0400

    234

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
        """,
     'author' : 'M.Gah',
     'website': '',
-    'depends': ['account','stock','base','sale','sale_stock','l10n_cl_fe','product'],
+    'depends': ['account','stock','base','sale','sale_stock','l10n_cl_fe','product','contacts'],
     'data': [
             "security/groups.xml",
             "security/ir.model.access.csv",
