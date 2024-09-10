ec3e9e1cd619ef4c3653a04b461612bcf0026d76
mgah77 <mauricio.gah@gmail.com>
2024-09-10 16:07:12 -0300

    p

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+from . import partner
 from . import sale_order
 from . import stock_picking
 from . import resumen
---
+++ models/partner.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class res_partner_customer_provider(models.Model):
+    _inherit = 'res.partner'
+
+    is_customer = fields.Boolean(string ='Cliente',default=False)
+    is_provider = fields.Boolean(string ='Proveedor',default=False)
+    
