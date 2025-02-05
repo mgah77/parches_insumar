a5f2cef76569c68d51cebf8618c363bb6c08623c
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-05 16:45:50 -0300

    we

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from . import partner
 from . import sale_order
 from . import stock_picking
 from . import resumen
+from . import product_template
--- models/product_template.py
+++ models/product_template.py
@@ -3,15 +3,6 @@ from odoo import models, fields
 class ProductDepartment(models.Model):
     _inherit = 'product.template'  # Reemplaza 'your.model' por el nombre del modelo que deseas modificar
 
-    # Modifica el campo partner_id para agregar un filtro adicional
-    depto = fields.Selection([
-                            ('Inspeccion de balsa','Inspeccion de balsa'),
-                            ('Contenedores','Contenedores'),
-                            ('Válvulas','Válvulas'),
-                            ('Exintores','Exintores'),
-                            ('Equipos de seguridad','Equipos de seguridad'),
-                            ('Banco CO2','Banco CO2'),
-                            ('Textil','Textil')
-    ])
+
     exchange_ok = fields.Boolean(string='Exchange', default=False)
 
