616ed4471befd9ccb62ccef277feb41a0307aeaa
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-05 16:47:11 -0300

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -4,5 +4,5 @@ class ProductDepartment(models.Model):
     _inherit = 'product.template'  # Reemplaza 'your.model' por el nombre del modelo que deseas modificar
 
 
-    exchange_ok = fields.Boolean(string='Exchange', default=False)
+    exchange_ok = fields.Boolean(string='Para Reemplazo', default=False)
 
