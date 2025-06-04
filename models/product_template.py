8c17fa412f2a6275f3d11e0c1f1ebf9a2c9b3d29
mgah77 <mauricio.gah@gmail.com>
2025-06-04 17:25:50 -0400

    Revert "agrega margen"
    This reverts commit b1201da83595142ce6c4e6db047658269a92acf3.

--- models/product_template.py
+++ models/product_template.py
@@ -5,20 +5,3 @@ class ProductDepartment(models.Model):
 
 
     exchange_ok = fields.Boolean(string='Para Reemplazo', default=False)
-
-    margen = fields.Float(
-        string='Margen de utilidad (%)',
-        compute='_compute_margen',
-        store=True,
-        digits=(16, 2)  # 2 decimales
-    )
-
-    @api.depends('standard_price', 'list_price')
-    def _compute_margen(self):
-        for product in self:
-            if product.standard_price > 0 and product.list_price > 0:
-                # Fórmula: ((Precio venta - Precio costo) / Precio costo) * 100
-                product.margen = ((product.list_price - product.standard_price) / product.standard_price) * 100
-            else:
-                product.margen = 0.0
-
