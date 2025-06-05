1f22964197e2b56e8c21a346ec63551d68a4ffab
mgah77 <mauricio.gah@gmail.com>
2025-06-05 16:12:01 -0400

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -6,4 +6,13 @@ class ProductDepartment(models.Model):
 
     exchange_ok = fields.Boolean(string='Para Reemplazo', default=False)
 
-    margenes = fields.Float(string='Margen', store=True)
+    margenes = fields.Float(string='Margen', store=True, compute='_compute_margen')
+
+    @api.depends('standard_price', 'list_price')
+    def _compute_margen(self):
+        for product in self:
+            if product.standard_price > 0 and product.list_price > 0:
+                # Fórmula: ((Precio venta - Precio costo) / Precio costo) * 100
+                product.margenes = ((product.list_price - product.standard_price) / product.standard_price) * 100
+            else:
+                product.margenes = 0.0
