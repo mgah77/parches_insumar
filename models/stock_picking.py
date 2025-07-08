6b97d6d892968cf5ff2c8db57de06ecd5f0261fe
mgah77 <mauricio.gah@gmail.com>
2025-07-08 11:31:53 -0400

    Update stock_picking.py

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -9,3 +9,22 @@ class stock_picking_custom_kanban(models.Model):
         for record in self:
             record['user_warehouse']=self.env.user.property_warehouse_id
             return
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    user_stock_location_id = fields.Many2one(
+        'stock.location',
+        compute='_compute_user_stock_location',
+        store=False
+    )
+
+    @api.depends('picking_type_id')
+    def _compute_user_stock_location(self):
+        for record in self:
+            warehouse = self.env.user.warehouse_id
+            if warehouse:
+                record.user_stock_location_id = warehouse.lot_stock_id
+            else:
+                record.user_stock_location_id = False
