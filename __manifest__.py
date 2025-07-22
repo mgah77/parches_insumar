1105000df99dc6e68c5c677550e926fefe0ccefd
mgah77 <mauricio.gah@gmail.com>
2025-07-22 15:41:59 -0400

    crm xml

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
        """,
     'author' : 'M.Gah',
     'website': '',
-    'depends': ['account','stock','base','sale','sale_stock','sale_management','l10n_cl_fe','product','contacts','utm','hr'],
+    'depends': ['account','stock','base','sale','sale_stock','sale_management','l10n_cl_fe','product','contacts','utm','hr','crm'],
     'data': [
             "security/groups.xml",
             "security/ir.model.access.csv",
@@ -23,7 +23,8 @@
             "views/configuration_menu.xml",
             "views/partner.xml",
             "views/calendar.xml",
-            "views/sale_template.xml"
+            "views/sale_template.xml",
+            "views/crm.xml"
     ],
     'assets': {
         'web.report_assets_common': [
